dc398195562aeeb7cd009e690fb4d7a98438b026
SATHEESH NAYAKA K A <92908130+sathishnayaka@users.noreply.github.com>
2021-11-25 19:27:30 +0530

    all_test_cases_passed

--- assignments/favourite_movie/favourite_movie.js
+++ assignments/favourite_movie/favourite_movie.js
@@ -2,6 +2,16 @@ const movies = [];
 
 function favouriteMovie(operation, movie) {
     // Write your code here
+    if(operation=="add"){
+        movies.push(movie);
+    }
+    else{
+        if(movies.length==0){
+            return movies
+        }
+        movies.pop();
+    }
+    return movies;
 }
 
 module.exports = favouriteMovie;
